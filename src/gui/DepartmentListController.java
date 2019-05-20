007b09638a6bf0125b98ae87426923aabc4e1fce
Iago Bontempo <iagobontempo@hotmail.com>
2019-05-20 13:24:54 -0300

    Obersers design patter to update tableview

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
@@ -26,10 +27,10 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable {
+public class DepartmentListController implements Initializable, DataChangeListener {
 
 	private DepartmentService service;
-	
+
 	@FXML
 	private TableView<Department> tableViewDepartment;
 	@FXML
@@ -37,36 +38,35 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private TableColumn<Department, String> tableColumnName;
 	@FXML
-	private Button btNew; 
-	
+	private Button btNew;
+
 	private ObservableList<Department> obsList;
-	
+
 	@FXML
 	public void onBtNewAction(ActionEvent event) {
 		Stage parentStage = Utils.currentStage(event);
 		Department obj = new Department();
 		createDialogForm(obj, "/gui/DepartmentForm.fxml", parentStage);
 	}
-	 
+
 	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
 	}
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		initializeNodes();			
+		initializeNodes();
 	}
-
 
 	private void initializeNodes() {
-		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id")); 				// para iniciar valores na tabela
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id")); // para iniciar valores na tabela
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
-		
-		Stage stage = (Stage) Main.getMainScene().getWindow();								// para pegar a janela
-		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());				// para definir que a tableView 
-																							// acompanhe a tela
+
+		Stage stage = (Stage) Main.getMainScene().getWindow(); // para pegar a janela
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty()); // para definir que a tableView
+																				// acompanhe a tela
 	}
-	
+
 	public void updateTableView() {
 		if (service == null) {
 			throw new IllegalStateException("Service was null");
@@ -75,18 +75,18 @@ public class DepartmentListController implements Initializable {
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
 	}
-	
-	
+
 	private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
-			
+
 			DepartmentFormController controller = loader.getController();
 			controller.setDeparment(obj);
 			controller.setDepartmentService(new DepartmentService());
+			controller.subscribeDataChangeListener(this);
 			controller.updateFormData();
-			
+
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Enter department data:");
 			dialogStage.setScene(new Scene(pane));
@@ -94,11 +94,15 @@ public class DepartmentListController implements Initializable {
 			dialogStage.initOwner(parentStage);
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
-			
+
 		} catch (IOException e) {
 			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
+
+	@Override
+	public void onDataChanged() {
+		updateTableView();
+	}
 
 }
